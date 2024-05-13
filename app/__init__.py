534dfdb452552931ffe921cbbc237fe983de7c3d
Kappxs <montesouzajoao@gmail.com>
2024-05-13 02:31:48 -0300

    teste

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 app = Flask(__name__)
 #URI DE CONEXÃO
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 db = SQLAlchemy(app)
+
+
+Manager= manager(app)
 
 
 from app.controllers import default
