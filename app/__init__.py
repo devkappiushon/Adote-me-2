2dd68a8b12aa5efbd5ab6d89e5b9a1f5827c30f0
Kappxs <montesouzajoao@gmail.com>
2024-05-26 01:18:40 -0300

    AJUSTANDO CSS/HTML

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
-
 
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-lm = LoginManager(app)
 
 from app.controllers import default
 
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,8 +3,13 @@ from flask import render_template
 from app import app
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login2.html")
 
 
 
